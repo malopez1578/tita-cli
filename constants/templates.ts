5f79409776e5f8c3045b4df4b8fb5feaf23aa82c
Miguel López <miguello@titamedia.com>
2025-06-11 18:05:56 -0500

    fix: Correct theme URL in URL_TEMPLATES for accurate repository reference
    Signed-off-by: Miguel López <miguello@titamedia.com>

--- constants/templates.ts
+++ constants/templates.ts
@@ -1,4 +1,5 @@
 export const URL_TEMPLATES = {
     'components': 'git@gitlab.com:titadev/tita-quality/quality-template.git',
-    'theme': 'git@gitlab.com:titadev/tita-quality/quality-template.git'
+    'theme': 'git@gitlab.com:titadev/tita-quality/quality-template-theme.git',
+    "theme-b2b": 'git@gitlab.com:titadev/tita-quality/quality-template-theme-b2b.git'
 }
