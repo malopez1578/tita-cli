fd95f656cf642d667aa753a0e133572d70bbe74b
Miguel López <miguello@titamedia.com>
2025-06-11 18:06:17 -0500

    fix: Update theme URL in URL_TEMPLATES and add theme-b2b entry
    Signed-off-by: Miguel López <miguello@titamedia.com>

--- dist/constants/templates.d.ts
+++ dist/constants/templates.d.ts
@@ -1,5 +1,6 @@
 export declare const URL_TEMPLATES: {
     components: string;
     theme: string;
+    "theme-b2b": string;
 };
 //# sourceMappingURL=templates.d.ts.map
--- dist/constants/templates.js
+++ dist/constants/templates.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.URL_TEMPLATES = void 0;
 exports.URL_TEMPLATES = {
     'components': 'git@gitlab.com:titadev/tita-quality/quality-template.git',
-    'theme': 'git@gitlab.com:titadev/tita-quality/quality-template.git'
+    'theme': 'git@gitlab.com:titadev/tita-quality/quality-template-theme.git',
+    "theme-b2b": 'git@gitlab.com:titadev/tita-quality/quality-template-theme-b2b.git'
 };
 //# sourceMappingURL=templates.js.map
